63d47b7fa7c0813ba5d13193ce2a826a831ac19f
olivier lamy <olamy@apache.org>
2016-10-17 15:55:23 +1100

    make port dynamique, add upload file path
    Signed-off-by: olivier lamy <olamy@apache.org>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # jetty-load-generator-app
+
 Jetty Spring boot type app for Jetty Load Testing
+
+`java -jar target/app-0.0.1-SNAPSHOT.jar --port=9090`
+
+File upload`curl -vvv -X PUT  -F file=@pom.xml http://localhost:9090/upload`
+
+
+
+
--- src/main/java/com/webtide/jetty/load/generator/web/HelloWorldController.java
+++ src/main/java/com/webtide/jetty/load/generator/web/HelloWorldController.java
@@ -1,14 +1,21 @@
 package com.webtide.jetty.load.generator.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HelloWorldController
 {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( HelloWorldController.class );
 
     @GetMapping( "/helloworld" )
     @ResponseBody
@@ -19,16 +26,27 @@ public class HelloWorldController
 
     @GetMapping( "/hello/{who}" )
     @ResponseBody
-    public String helloWhoFromPath( @PathVariable String who)
+    public String helloWhoFromPath( @PathVariable String who )
     {
         return "Hello " + who;
     }
 
     @GetMapping( "/hello" )
     @ResponseBody
-    public String helloWhoFromRequestPath( @RequestParam(name = "who") String who)
+    public String helloWhoFromRequestPath( @RequestParam( name = "who" ) String who )
     {
         return "Hello " + who;
+    }
+
+
+    @PutMapping( "/upload" )
+    public String uploadFile( @RequestParam( "file" ) MultipartFile file, RedirectAttributes redirectAttributes )
+        throws Exception
+    {
+        redirectAttributes.addAttribute( "message", //
+                                         "uploaded:" + file.getOriginalFilename() + ",size:" + file.getBytes().length );
+
+        return "redirect:/{message}";
     }
 
 }
