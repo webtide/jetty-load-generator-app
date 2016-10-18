f7adb744ed9dfcc297f21ea7968733d81df1eed4
olivier lamy <olamy@apache.org>
2016-10-18 14:46:48 +1100

    log file path with jetty port
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/main/java/com/webtide/jetty/load/generator/AppApplication.java
+++ src/main/java/com/webtide/jetty/load/generator/AppApplication.java
@@ -1,8 +1,9 @@
 package com.webtide.jetty.load.generator;
 
 import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
@@ -11,7 +12,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
@@ -21,6 +21,8 @@ import java.util.Collections;
 @SpringBootApplication
 public class AppApplication
 {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( AppApplication.class );
 
     private static ServerConnector serverConnector;
 
@@ -38,10 +40,13 @@ public class AppApplication
         Path path = Paths.get( "jetty.local.port" );
 
         Files.write( path, //
-                     Collections.singleton(Integer.toString( serverConnector.getLocalPort())), //
+                     Collections.singleton( Integer.toString( serverConnector.getLocalPort() ) ), //
                      StandardOpenOption.TRUNCATE_EXISTING, //
                      StandardOpenOption.WRITE, //
-                     StandardOpenOption.CREATE);
+                     StandardOpenOption.CREATE );
+
+        LOGGER.info( "jetty port write to file: {}", path );
+
     }
 
     //olamy: hackhish way to get local port when starting with random
@@ -58,15 +63,14 @@ public class AppApplication
             {
                 Collection<JettyServerCustomizer> serverCustomizers = super.getServerCustomizers();
 
-                serverCustomizers.add(
-                    server -> {
-                        Connector connector = server.getConnectors()[0];
-                        if ( connector instanceof ServerConnector )
-                        {
-                            AppApplication.serverConnector = (ServerConnector) connector;
-                        }
-                    }
-                );
+                serverCustomizers.add( server ->
+                                       {
+                                           Connector connector = server.getConnectors()[0];
+                                           if ( connector instanceof ServerConnector )
+                                           {
+                                               AppApplication.serverConnector = (ServerConnector) connector;
+                                           }
+                                       } );
 
                 return serverCustomizers;
             }
