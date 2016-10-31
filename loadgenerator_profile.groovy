6ffc2b6b85ced482f002b4bc6e2e378d18699303
olivier lamy <olamy@apache.org>
2016-10-31 14:09:51 +1100

    fix poms
    Signed-off-by: olivier lamy <olamy@apache.org>

--- loadgenerator_profile.groovy
+++ loadgenerator_profile.groovy
@@ -1,13 +1,19 @@
 return new ResourceProfile(new Resource( "index.html",
                                          new Resource( "/css/bootstrap.css",
-                                                       new Resource( "/css/bootstrap-theme.css" )
+                                                       new Resource( "/css/bootstrap-theme.css" ),
+                                                       new Resource( "/js/jquery-3.1.1.min.js"),
+                                                       new Resource( "/js/jquery-3.1.1.min.js"),
+                                                       new Resource( "/js/jquery-3.1.1.min.js"),
+                                                       new Resource( "/js/jquery-3.1.1.min.js")
                                          ),
                                          new Resource( "/js/bootstrap.js" ,
                                                        new Resource( "/js/bootstrap.js" ),
                                                        new Resource( "/js/bootstrap.js" ),
                                                        new Resource( "/js/bootstrap.js" )
                                          ),
-                                         new Resource( "/helloworld" ),
+                                         new Resource( "/hello" ),
+                                         new Resource( "/hello?name=foo" ),
+                                         new Resource( "/hello?name=foo" ),
                                          new Resource( "/upload" ).size(8192),
                                          )
 );
